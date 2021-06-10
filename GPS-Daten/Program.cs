7a8c02fc87e7a5a4ab703e554e9eb81db8a838d7
Michael Beck <mbeck@lutzundgrub.de>
2021-06-10 16:03:20 +0200

    Gps-Daten

---
+++ GPS-Daten/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GPS_Daten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal dezimalMinute;
+            int minuten, sekunden;
+
+
+            do
+            {
+                //Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write("Bitte Dezimalminuten angeben: ");
+                string eingabe = Console.ReadLine(); // Bsp: 21,62083
+                dezimalMinute = Convert.ToDecimal(eingabe);
+
+                minuten = (int)dezimalMinute; // 21
+                sekunden = (int)((dezimalMinute - minuten) * 60);  // 21,62083 - 21 => 0,62083 -> mal 60 
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("{0}' {1}\"", minuten, sekunden); // 21' 37"
+            } while (dezimalMinute != 0);
+
+        }
+    }
+}
