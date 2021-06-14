0b611dafb013cd19cec4e206fc5d66780acec016
Michael Beck <mbeck@lutzundgrub.de>
2021-06-14 10:06:49 +0200

    Schraubenhersteller hinzugefügt

---
+++ Schraubenhersteller/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Schraubenhersteller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bestellmenge;
+            double rabatt = 0.05;
+            double versandgebuehr = 5.00;
+            double preisProSchraube = 0.20;
+            double Gesamtpreis;
+
+            Console.Write("Bitte Schraubenmenge angeben: ");
+            string eingabe = Console.ReadLine();
+            bestellmenge = Convert.ToInt32(eingabe);
+
+            if (bestellmenge < 1000)
+            {
+                //wahr, true
+                Gesamtpreis = (bestellmenge * preisProSchraube) + versandgebuehr;
+            }
+            else
+            {
+                //falsch, false
+                Gesamtpreis = (bestellmenge * preisProSchraube) - (bestellmenge * preisProSchraube * rabatt);
+            }
+
+            Console.WriteLine($"Der Gesamtpreis beträgt {Gesamtpreis:f2} Euro");
+
+
+
+            Console.WriteLine("Bitte Taste drücken ...");
+            Console.ReadKey();
+        }
+    }
+}
